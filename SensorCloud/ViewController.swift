412751468b78d97e4d311826ac4864ed07ac8d5b
Darko <qq1572055493@icloud.com>
2018-02-04 15:15:11 +0800

    Initial Commit

---
+++ SensorCloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SensorCloud
+//
+//  Created by Darko on 2018/2/4.
+//  Copyright © 2018年 Darko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
